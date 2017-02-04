aba0b5ce0534986bcc7a31c2ac01e56cf153edda
Seidor-PC\Seidor <ruiz27@gmail.com>
2017-02-04 14:58:33 +0100

    Se adiciona print de prueba

--- jenkins-maven-project/src/main/java/com/prueba/maven/JenkinsMavenProjectApplication.java
+++ jenkins-maven-project/src/main/java/com/prueba/maven/JenkinsMavenProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsMavenProjectApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hola");
 		SpringApplication.run(JenkinsMavenProjectApplication.class, args);
 	}
 }
