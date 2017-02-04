f0896f98ba3c3b3ebe81d79d61535d6923c4bf27
Seidor-PC\Seidor <ruiz27@gmail.com>
2017-02-04 14:43:00 +0100

    Primer commit

---
+++ jenkins-maven-project/src/main/java/com/prueba/maven/JenkinsMavenProjectApplication.java
@@ -0,0 +1,12 @@
+package com.prueba.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsMavenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsMavenProjectApplication.class, args);
+	}
+}
